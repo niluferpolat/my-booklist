8d8e571d7b64b5518b9efbc6a75ffa6875b5c8a7
unknown <niluferpolat2@gmail.com>
2025-01-05 14:47:27 +0300

    added some files

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from 'react-native';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+
+function HomeScreen() {
+  const insets = useSafeAreaInsets();
+  return (
+    <View style={{ flex: 1, paddingTop: insets.top }}>
+      <Text style={{ fontSize: 28 }}>Contefdsfdsfsnt is in safe area.</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <HomeScreen />
+    </SafeAreaProvider>
+  );
+}
